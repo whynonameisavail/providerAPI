21d2bc588002a93f31f09bc2854e357501b7fc5e
YunyiZheng <zheng59@illinois.edu>
2023-01-23 01:14:22 -0500

    finish api body

---
+++ src/main/java/com/example/providerapi/ProviderapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.providerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/providerapi/ProviderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.providerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
